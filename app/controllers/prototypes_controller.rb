3f2a462f20381cce800c31fbab6af89946b75fbf
sorakanda <115918500+sorakanda@users.noreply.github.com>
2023-02-17 07:08:49 +0900

    first commit3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :edit, :desroy, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   
 
   def index
