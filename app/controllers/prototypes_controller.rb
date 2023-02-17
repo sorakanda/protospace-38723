bdf9bd8f3355bd0229e759452db5f97fd1d13167
sorakanda <115918500+sorakanda@users.noreply.github.com>
2023-02-17 12:09:34 +0900

    first_commit4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :move_to_index, only: :edit
 
   def index
     @prototypes = Prototype.all
@@ -28,9 +28,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to action: index
-    end
   end
 
   def update
@@ -52,5 +49,11 @@ class PrototypesController < ApplicationController
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
